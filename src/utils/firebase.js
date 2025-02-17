8752a676e62962df326d53954927cc4273b9c75d
amaanxmd <amnxmd@gmail.com>
2025-02-18 02:02:31 +0530

    completed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -72,7 +72,7 @@ export function signOutfromAccount(navigate){
   const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'https://amaanxmd.github.io/GeminiFlix/#/browse',
+    url: 'https://gemini-flix-orpin.vercel.app/',
     // This must be true.
     handleCodeInApp: true,
     // iOS: {
