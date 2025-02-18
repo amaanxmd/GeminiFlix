6ac2333257f6f6f119d4fbfce3dec99f91502c6f
amaanxmd <amnxmd@gmail.com>
2025-02-18 16:45:00 +0530

    completed

--- src/customHooks/useSearchMovies.js
+++ src/customHooks/useSearchMovies.js
@@ -1,45 +1,63 @@
-import { useEffect } from 'react'
-import { options } from '../utils/constant'
-import { useDispatch } from 'react-redux'
-import { addfetchedquery,handleErrorFromTmdb } from '../utils/fetchedmoviefromqueryslice'
-
+import { useEffect } from "react";
+import { options } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import {
+  addfetchedquery,
+  handleErrorFromTmdb,
+} from "../utils/fetchedmoviefromqueryslice";
 
 const useSearchMovies = (movielist) => {
-  const dispatch =useDispatch()
+  const dispatch = useDispatch();
 
-   async function searchMovies(){
-    
-    try{
-     
-    if (!movielist?.movieList)return
-   
-   const moviedata= await Promise.all( movielist.movieList.map(async(data)=>{ const movie = await fetch(`https://api.themoviedb.org/3/search/movie?query=${data.title}&include_adult=true&language=en-US&page=1`,
-                options);
-           const json =await movie.json() ;
-           
-            return json.results}))
-            const lengthsofsubarray = ((moviedata.map((subarray)=>subarray.length)))
-            const maxElementinArray = Math.max(...lengthsofsubarray)
-            const newmovieList = Array.from({ length: maxElementinArray }, (_, index) => 
-             
-               moviedata.map((arr) => arr[index]===undefined?"":arr[index] ).filter((arr)=>arr!==''))
-               const totalMovieData = newmovieList.flat()//could contain duplicates
-               const idsOfMovieData =totalMovieData.map((data)=>data.id)
-               const booleanMovieData = idsOfMovieData.map((data,index)=>idsOfMovieData.indexOf(data)===index?true:false)
-               const updatedMovieData =totalMovieData.filter((_,index)=>booleanMovieData[index]===true)
+  async function searchMovies() {
+    try {
+      if (!movielist?.movieList) return;
 
-             
-      dispatch(addfetchedquery(updatedMovieData))
+      const moviedata = await Promise.all(
+        movielist.movieList.map(async (data) => {
+          try{
+
           
-} catch(e){
-  dispatch(handleErrorFromTmdb())
-  console.log(e)
-}
+          
+          const movie = await fetch(
+            `https://api.themoviedb.org/3/search/movie?query=${data.title}&include_adult=true&language=en-US&page=1`,
+            options
+          );
+          const json = await movie.json();
+
+          return json.results;
+    }catch(e){
+     return null
+    }})
+      );
+      const lengthsofsubarray = moviedata.map((subarray) => subarray.length);
+      const maxElementinArray = Math.max(...lengthsofsubarray);
+      const newmovieList = Array.from(
+        { length: maxElementinArray },
+        (_, index) =>
+          moviedata
+            .map((arr) => (arr[index] === undefined ? "" : arr[index]))
+            .filter((arr) => arr !== "")
+      );
+      const totalMovieData = newmovieList.flat(); //could contain duplicates
+      const idsOfMovieData = totalMovieData.map((data) => data.id);
+      const booleanMovieData = idsOfMovieData.map((data, index) =>
+        idsOfMovieData.indexOf(data) === index ? true : false
+      );
+      const updatedMovieData = totalMovieData.filter(
+        (_, index) => booleanMovieData[index] === true
+      );
+
+      dispatch(addfetchedquery(updatedMovieData));
+    } catch (e) {
+      dispatch(handleErrorFromTmdb());
+      console.log(e);
+    }
   }
-  
- useEffect(()=>{searchMovies()},[movielist?.movieList?.length])
 
-}
+  useEffect(() => {
+    searchMovies();
+  }, [movielist?.movieList?.length]);
+};
 
-export default useSearchMovies
-
+export default useSearchMovies;
