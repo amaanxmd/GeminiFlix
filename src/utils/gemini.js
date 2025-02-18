f0fd9df041f7e8b62c47577dfdb903428fa08f99
amaanxmd <amnxmd@gmail.com>
2025-02-18 20:14:45 +0530

    completed

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -3,7 +3,7 @@ import { geminikey } from "./constant";
 
 
 const genAI  = new GoogleGenerativeAI(geminikey)
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-pro-experimental-02-05" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 export  async function gemini(query,dispatch,setquery,setloadingMessage){
   
